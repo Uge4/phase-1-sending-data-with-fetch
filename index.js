37f7086756f21bc4cb0d8e97aef0071cb841f18e
eugene <eugeneholenstein@gmail.com>
2023-01-06 01:11:02 +1100

    completed sending datga with fetch

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email){
+    let user = {}
+    user.name = name
+    user.email = email
+    
+       
+
+    let response = fetch('http://localhost:3000/users', {
+        
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "accept" : "application/json",
+        },
+        body: JSON.stringify(user)
+            
+    })
+    .then(res => res.json())
+    .then(user => document.body.innerHTML = user.id)
+    .catch(error => {
+        let message = 'Unauthorized Access'
+        document.body.innerHtml = message
+    })
+
+    return response
+    
+}
+
+
+submitData("Steve", "steve@steve.com")
